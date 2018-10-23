fcc4faabd285dc88277848d510112cb72e1174d8
Max Timkovich <mtimkovich@gmail.com>
2018-10-23 22:35:29 +0000

    interactive prompt

--- tobot.py
+++ tobot.py
@@ -14,10 +14,19 @@ if not initialized:
 
 tourney_url = 'https://challonge.com/tobot_test'
 tourney_name = util_challonge.extract_tourney_name(tourney_url)
-# Cache participants so we only don't have to make a network call everytime
+# Cache participants so we only don't have to make a network call every time
 # we want to get tags.
 participants = challonge.participants.index(tourney_name)
+matches = challonge.matches.index(tourney_name)
+open_matches = []
 
+
+def update_matches():
+    global open_matches
+    matches = challonge.matches.index(tourney_name)
+    match_infos = [MatchInfo(m) for m in matches]
+    # Sort matches by suggested play order
+    open_matches = sorted(m for m in match_infos if m.state == 'open')
 
 def player_tag_from_id(id):
     """
@@ -35,7 +44,14 @@ def player_tag_from_id(id):
 
 
 class MatchInfo(object):
+    """
+    TODO: Mark match as in progress.
+        * the challonge API doesn't support, so we'll need to store a local
+              store of what matches are in progress.
+    TODO: Report scores.
+    """
     def __init__(self, match):
+        self.match_id = match['id']
         self.id1 = match['player1_id']
         self.id2 = match['player2_id']
         self.state = match['state']
@@ -47,41 +63,66 @@ class MatchInfo(object):
             self.player1_tag = player_tag_from_id(self.id1)
             self.player2_tag = player_tag_from_id(self.id2)
 
-    def __repr__(self):
+    def identifier_str(self):
+        """The match identifier with an * if it's in progress."""
         underway = ' '
         if self.underway_at is not None:
             underway = '*'
-        return '{}{}: {} vs {}'.format(underway,
+
+        return '{}{}:'.format(underway, self.identifier)
+
+    def __repr__(self):
+        return '{}{}: {} vs {}'.format(self.identifier_str(),
                                      self.identifier,
                                      self.player1_tag,
                                      self.player2_tag)
 
     def _parts(self):
-        underway = ' '
-        if self.underway_at is not None:
-            underway = '*'
-        return [underway+self.identifier+':',
+        return [self.identifier_str(),
                 self.player1_tag,
                 'vs',
                 self.player2_tag]
 
+    def __lt__(self, other):
+        return self.suggested_play_order < other.suggested_play_order
+
 
 def column_print_matches(match_infos):
-    PADDING = 1
+    """Print match info in nice pretty columns."""
     rows = [m._parts() for m in match_infos]
     widths = [max(map(len, col)) for col in zip(*rows)]
     for row in rows:
         print(" ".join((val.ljust(width) for val, width in zip(row, widths))))
 
 
-while True:
-    matches = challonge.matches.index(tourney_name)
-    match_infos = [MatchInfo(m) for m in matches]
-    open_matches = [m for m in match_infos if m.state == 'open']
+def nop():
+    pass
 
-    # Clear screen
-    subprocess.call(['clear'])
+
+def prompt():
+    def help_prompt():
+        for cmd, help in commands.items():
+            print('  {} - {}'.format(cmd, help[0]))
+
+    commands = {
+        'u': ('update match list', update_matches),
+        'h': ('print help', help_prompt),
+        '?': ('print help', help_prompt),
+        'q': ('quit', sys.exit),
+        '*': ('toggle in progress status', nop),
+        'r': ('report the result of a match. e.g. `r I 2-0`', nop),
+    }
+
+    ch = input('> ').lower()
+    if ch in commands:
+        commands[ch][1]()
+    else:
+        print('invalid command: {}'.format(ch))
+    print()
+
+
+while True:
+    update_matches()
 
     column_print_matches(open_matches)
-
-    time.sleep(5)
+    prompt()
