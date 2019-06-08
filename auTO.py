e15b3eb99b494975f780626bd4312d9480c5c8c9
Max Timkovich <mtimkovich@gmail.com>
2019-06-07 22:58:25 -0700

    auTO server

--- auTO.py
+++ auTO.py
@@ -83,22 +83,6 @@ class Command(object):
         return self.index < other.index
 
 
-class MatchEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, MatchInfo):
-            return {
-                'id': obj.id,
-                'player1_id': obj.player1_id,
-                'player2_id': obj.player2_id,
-                'player1_tag': obj.player1_tag,
-                'player2_tag': obj.player2_tag,
-                'state': obj.state,
-                'in_progress': obj.in_progress,
-                'suggested_play_order': obj.suggested_play_order,
-            }
-        else:
-            return json.JSONEncoder.default(self, obj)
-
 class MatchInfo(object):
     """
     TODO: Keep track of available setups.
---
+++ auTO_server.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, abort, jsonify, request
+import json
+
+from auTO
+
+auto = Blueprint('auTO', __name__)
+
+@auto.route('/'):
+    # TODO: Load Vue page
+    pass
+
+class MatchEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, auTO.MatchInfo):
+            return {
+                'id': obj.id,
+                'player1_id': obj.player1_id,
+                'player2_id': obj.player2_id,
+                'player1_tag': obj.player1_tag,
+                'player2_tag': obj.player2_tag,
+                'state': obj.state,
+                'in_progress': obj.in_progress,
+                'suggested_play_order': obj.suggested_play_order,
+            }
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+@auto.route('/api/get_matches', method=['POST']):
+    r = request.get_json()
+    to = auTo.auTO(r.url)
+
+    return jsonify(to.open_matches, cls=MatchEncoder)
+
+@auto.route('/api/report', method=['POST']):
+    pass
