b786ac175c6d8ac358e54031b81c34ba16248a17
Max Timkovich <mtimkovich@gmail.com>
2019-06-10 17:14:08 -0700

    accidentally ignored js src

---
+++ auto/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{matches}}
+  </div>
+</template>
+
+<script>
+from axios import 'axios';
+
+export default {
+  name: 'app',
+
+  data() {
+    return {
+      matches: [],
+    }
+  },
+
+  mounted() {
+    axios.post('http://localhost:5000/auTO/api/get_matches', {
+      url: 'https://mtvmelee.challonge.com/120'
+    })
+    .then(response => (this.matches = response))
+  }
+}
+</script>
